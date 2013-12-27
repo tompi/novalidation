01270ae1e19bdb94d1978f8f534ebe6117b42324
Thomas Haukland <thomas.haukland@gmail.com>
2013-12-27 23:44:40 +0100

    grunt automation

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+	require('grunt-load')(grunt).loadNpmTasks();
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+		},
+		jasmine: {
+			pivotal: {
+				src: 'src/**/*.js',
+				options: {
+					specs: 'tests/*Spec.js',
+					vendor: 'bower_components/jquery/jquery.js'
+				}
+			}
+		},
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', ['jasmine', 'jshint', 'uglify']);
+};
