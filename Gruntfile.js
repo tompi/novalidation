bc92131a85cbd8ffc9ff381d3940a70136015770
Thomas Haukland <thomas.haukland@gmail.com>
2015-03-07 10:28:26 +0100

    Removed bower.json, renamed jasmine to test

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,5 +27,6 @@ module.exports = function(grunt) {
   });
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'jasmine', 'uglify']);
+  grunt.registerTask('test', ['jasmine']);
+  grunt.registerTask('default', ['jshint', 'test', 'uglify']);
 };
